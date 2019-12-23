3690996eee6b9751da2879839418d36a10b25271
Nora Liu <tzuyuanliu0830@gmail.com>
2019-12-23 11:00:51 +0800

    Updates

---
+++ precache-manifest.5b7e9c7969b2c832a2cc2ff5cba5c328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd376f4ba37af443b8936fdf7625c789",
+    "url": "/cws-web-flow/index.html"
+  },
+  {
+    "revision": "bc26a2c844eef3f2cf23",
+    "url": "/cws-web-flow/static/css/main.5f7a07f3.chunk.css"
+  },
+  {
+    "revision": "afca26078671a34ab657",
+    "url": "/cws-web-flow/static/js/2.5bd8d369.chunk.js"
+  },
+  {
+    "revision": "bc26a2c844eef3f2cf23",
+    "url": "/cws-web-flow/static/js/main.7310a49f.chunk.js"
+  },
+  {
+    "revision": "bb4238ffb85b176d719c",
+    "url": "/cws-web-flow/static/js/runtime-main.56d0d839.js"
+  }
+]);
