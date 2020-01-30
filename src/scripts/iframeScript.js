1a4c962be8b9661c4e40f572191a96b2eb98e784
Nora Liu <tzuyuanliu0830@gmail.com>
2020-01-30 11:15:13 +0800

    modify domain

--- src/scripts/iframeScript.js
+++ src/scripts/iframeScript.js
@@ -1,73 +1,73 @@
-import { Component } from 'react'
+import { Component } from 'react';
 
 export default class IframeComponent extends Component {
-  constructor(props) {
-    super(props)
-    if (window.parent !== window) {
-      this.tabReady = false
-      this.bc = new BroadcastChannel('coolwallets')
-      this.setupListeners()
-    }
-  }
+	constructor(props) {
+		super(props);
+		if (window.parent !== window) {
+			this.tabReady = false;
+			this.bc = new BroadcastChannel('coolwallets');
+			this.setupListeners();
+		}
+	}
 
-  setupListeners() {
-    // const tabDomain = 'https://coolbitx-technology.github.io/coolwallet-connect/#/'
-    const tabDomain = 'http://localhost:3000'
+	setupListeners() {
+		const tabDomain = 'https://coolbitx-technology.github.io/coolwallet-connect/#/';
+		// const tabDomain = 'http://localhost:3000'
 
-    // Open as IFRAME
-    onmessage = async ({ data, source, origin }) => {
-      if (data.target === 'CWS-IFRAME' && source === window.parent) {
-        // Open CoolWalletConnect in new tab.
-        const tab = this.openOnce(tabDomain, 'coolwallets-tab')
-        
-        tab.onbeforeunload = ()=>{
-          this.tabReady = false
-        }
+		// Open as IFRAME
+		onmessage = async ({ data, source, origin }) => {
+			if (data.target === 'CWS-IFRAME' && source === window.parent) {
+				// Open CoolWalletConnect in new tab.
+				const tab = this.openOnce(tabDomain, 'coolwallets-tab');
 
-        if (!this.tabReady) this.pingTab()
-        while (!this.tabReady) {
-          console.log(`blocking`)
-          await this.sleep(1000)
-        }
+				tab.onbeforeunload = () => {
+					this.tabReady = false;
+				};
 
-        data.target = 'CWS-TAB'
-        this.bc.postMessage(data, '*')
-      }
-    }
+				if (!this.tabReady) this.pingTab();
+				while (!this.tabReady) {
+					console.log(`blocking`);
+					await this.sleep(1000);
+				}
 
-    this.bc.onmessage = ({ data, source }) => {
-      if (data.target === 'tab-status' ) {
-        this.tabReady = data.ready
-      } else {
-        this.sendMessageToExtension(data)
-      }
-    }
-  }
+				data.target = 'CWS-TAB';
+				this.bc.postMessage(data, '*');
+			}
+		};
 
-  pingTab(){
-    this.bc.postMessage({ target: 'CWS-TAB', action: 'coolwallet-connection-check' })
-  }
+		this.bc.onmessage = ({ data, source }) => {
+			if (data.target === 'tab-status') {
+				this.tabReady = data.ready;
+			} else {
+				this.sendMessageToExtension(data);
+			}
+		};
+	}
 
-  sendMessageToExtension(msg) {
-    window.parent.postMessage(msg, '*')
-  }
+	pingTab() {
+		this.bc.postMessage({ target: 'CWS-TAB', action: 'coolwallet-connection-check' });
+	}
 
-  openOnce(url, target) {
-    var winref = window.open('', target, '', true)
+	sendMessageToExtension(msg) {
+		window.parent.postMessage(msg, '*');
+	}
 
-    // if the "target" window was just opened, change its url
-    if (winref.location.href === 'about:blank') {
-      winref.location.href = url
-    }
-    winref.focus()
-    return winref
-  }
+	openOnce(url, target) {
+		var winref = window.open('', target, '', true);
 
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+		// if the "target" window was just opened, change its url
+		if (winref.location.href === 'about:blank') {
+			winref.location.href = url;
+		}
+		winref.focus();
+		return winref;
+	}
 
-  render() {
-    return null
-  }
+	sleep(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
+
+	render() {
+		return null;
+	}
 }
